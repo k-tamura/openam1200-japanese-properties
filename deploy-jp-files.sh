d27ae9e6dcf1eda900d9d90b14083978d3540982
Kohei Tamura <ktamura.biz.80@gmail.com>
2015-10-23 13:35:27 +0900

    Change some command options.

--- deploy-jp-files.sh
+++ deploy-jp-files.sh
@@ -7,22 +7,22 @@ echo "Please input OpenAM installation directory. For example, /usr/share/tomcat
 read AM_DIR
 
 # Remove backup files if exist.
-rm -fr $AM_DIR/XUI/locales/ja/
-rm -fr $AM_DIR/policyEditor/locales/ja/
-rm -f $AM_DIR/XUI/locales/ja/translation.json
-rm -f $AM_DIR/policyEditor/locales/ja/translation.json
+rm -fr $AM_DIR/XUI/locales/ja
+rm -fr $AM_DIR/policyEditor/locales/ja
 rm -f $AM_DIR/WEB-INF/classes/*_ja.properties
-mv $AM_DIR/config/auth/default_ja_bak $AM_DIR/config/auth/default_ja
-mv $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js.bak $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js
-mv $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js.bak $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js
+\cp -fr $AM_DIR/config/auth/default_ja_bak $AM_DIR/config/auth/default_ja
+rm -f $AM_DIR/config/auth/default_ja/DeviceIdMatch.xml $AM_DIR/config/auth/default_ja/DeviceIdSave.xml
+rm -fr $AM_DIR/config/auth/default_ja_bak
+mv -f $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js.bak $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js
+mv -f $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js.bak $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js
 
 # Deploy Japanese properties, xml and json files.
 mkdir -p $AM_DIR/XUI/locales/ja/
 mkdir -p $AM_DIR/policyEditor/locales/ja/
 unzip openam1200-japanese-properties-master.zip
 cp -pr $AM_DIR/config/auth/default_ja $AM_DIR/config/auth/default_ja_bak
-mv $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js.bak
-mv $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js.bak
+mv -f $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js.bak
+mv -f $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js.bak
 cp openam1200-japanese-properties-master/openam-ui-policy/src/main/js/org/forgerock/openam/ui/policy/delegates/SiteConfigurationDelegate.js $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js
 cp openam1200-japanese-properties-master/openam-ui-ria/src/main/js/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js
 cp -r openam1200-japanese-properties-master/openam-ui-ria/src/main/resources/locales/ja/translation.json $AM_DIR/XUI/locales/ja/
--- undeploy-jp-files.sh
+++ undeploy-jp-files.sh
@@ -4,11 +4,11 @@ echo "Please input OpenAM installation directory. For example, /usr/share/tomcat
 read AM_DIR
 
 # Remove backup files if exist.
-rm -fr $AM_DIR/XUI/locales/ja/
-rm -fr $AM_DIR/policyEditor/locales/ja/
-rm -f $AM_DIR/XUI/locales/ja/translation.json
-rm -f $AM_DIR/policyEditor/locales/ja/translation.json
+rm -fr $AM_DIR/XUI/locales/ja
+rm -fr $AM_DIR/policyEditor/locales/ja
 rm -f $AM_DIR/WEB-INF/classes/*_ja.properties
-mv $AM_DIR/config/auth/default_ja_bak $AM_DIR/config/auth/default_ja
-mv $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js.bak $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js
-mv $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js.bak $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js
+\cp -fr $AM_DIR/config/auth/default_ja_bak $AM_DIR/config/auth/default_ja
+rm -f $AM_DIR/config/auth/default_ja/DeviceIdMatch.xml $AM_DIR/config/auth/default_ja/DeviceIdSave.xml
+rm -fr $AM_DIR/config/auth/default_ja_bak
+mv -f $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js.bak $AM_DIR/policyEditor/org/forgerock/openam/ui/policy/SiteConfigurationDelegate.js
+mv -f $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js.bak $AM_DIR/XUI/org/forgerock/openam/ui/common/delegates/SiteConfigurationDelegate.js
